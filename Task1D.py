718bf773c9032b003007c153da149fd60e33e1f2
Jiachen Liu <jl2264@cam.ac.uk>
2022-01-30 03:32:02 +0000

    30/01/2022

--- Task1D.py
+++ Task1D.py
@@ -6,33 +6,20 @@ from floodsystem.geo import rivers_with_station, stations_by_river
 from floodsystem.stationdata import build_station_list
 import floodsystem.station
 
-
-def rivers_monitored():
+def run():
     stations = build_station_list()
+
     rivers_full = rivers_with_station(stations)
     rivers_full.sort()
-    print(len(rivers_full),'First 10 - ', rivers_full[:10], '\n\n\n\n')
-    return rivers_full
-
-
-def stations_by_River_1D():
-    stations = build_station_list()
+    
     dict_stations_1D = stations_by_river(stations)
-    print ('River Aire - ', dict_stations_1D['River Aire'], '\n\n', 'River Cam - ', dict_stations_1D['River Cam'],'\n\n', 'River Thames - ', dict_stations_1D['River Thames'])
-    return dict_stations_1D
+
+    print('Numbers of rivers monitored : ', len(rivers_full), '\n First 10 - ', rivers_full[:10], '\n\n')
+    
+    print('River Aire : ', dict_stations_1D['River Aire'], '\n\n', 'River Cam : ', dict_stations_1D['River Cam'],'\n\n', 'River Thames : ', dict_stations_1D['River Thames'])
 
 
-
-def run():
-    rivers_monitored()
-    # Put code here that demonstrates functionality
 
 if __name__ == "__main__":
-    run()
-
-def run():
-    stations_by_River_1D()
-    # Put code here that demonstrates functionality
-
-if __name__ == "__main__":
+    print("\n *** Task 1D: rivers with a station(s) *** \n")
     run()
