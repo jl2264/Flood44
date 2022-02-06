9049cf696b9b66848a70258fdedf5ee293eaa08c
Melina Duer <md919@cam.ac.uk>
2022-02-06 23:13:31 +0000

    1c unit test

--- test_1C.py
+++ test_1C.py
@@ -2,7 +2,7 @@
 from floodsystem.geo import stations_within_radius
 from floodsystem.station import MonitoringStation
 
-def test_stations_by_distance():
+def test_stations_within_radius():
     
     stations = []
 
